fde91f9de9835605ecede9ba8055d851bc2f43be
zhukangli <276012105@qq.com>
2020-02-04 23:34:46 +0800

    461提交

--- src/main/java/com/conley/zhu/leetcode/Code461.java
+++ src/main/java/com/conley/zhu/leetcode/Code461.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Code461 {
     /**
      * 这个异或操作前面有题做过，这我轻松做出了
+     * 但是看了答案，人家 z%2用 z&1 代替，z/2 用 z>>1 代替，效率比我高
      */
     public int hammingDistance(int x, int y) {
         int z = x ^ y;
