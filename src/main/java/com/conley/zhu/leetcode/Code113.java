39702e3e4cccbd34bccb6c6bf4cc18ae0031203b
zhukangli <276012105@qq.com>
2020-02-06 21:41:32 +0800

    437提交

---
+++ src/main/java/com/conley/zhu/leetcode/Code113.java
@@ -0,0 +1,22 @@
+package com.conley.zhu.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code113 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    List<List<Integer>> list = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+       return null;
+
+    }
+
+
+}
--- src/main/java/com/conley/zhu/leetcode/Code437.java
+++ src/main/java/com/conley/zhu/leetcode/Code437.java
@@ -12,18 +12,26 @@ public class Code437 {
         对所有的子树，统计其所有的和为sum路径
      */
     public int pathSum(TreeNode root, int sum) {
-        return path(root,sum,0);
+        int res = path(root,sum) ;
+        if(root != null){
+            res += pathSum(root.left,sum);
+            res += pathSum(root.right,sum);
+        }
+        return res;
     }
 
-    public int path(TreeNode root, int sum,int res){
+
+    public int path(TreeNode root, int sum){
+        int res = 0;
         if (root == null){
             return res;
         }
         if(sum == root.val){
-            return  ++res;
+            //这里不能return，我就是这里return导致错了，因为还可以继续找下面的路径
+            ++res;
         }
-        res += path(root.left,sum - root.val,res);
-        res += path(root.right,sum - root.val,res);
+        res += path(root.left,sum - root.val);
+        res += path(root.right,sum - root.val);
         return res;
     }
 
