53b45ce81f05703d44e565698e016ccef599b608
zhukangli <276012105@qq.com>
2020-02-21 23:02:22 +0800

    114提交

--- src/main/java/com/conley/zhu/leetcode/Code114.java
+++ src/main/java/com/conley/zhu/leetcode/Code114.java
@@ -1,4 +1,27 @@
 package com.conley.zhu.leetcode;
 
 public class Code114 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    /**
+     * 后续遍历，把后一个的右指针指向前一个
+     * @param root
+     */
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        //这一步很重要，要把左边的去掉，因为是从后面遍历，不用担心改变子节点，因为后面的已经处理了，再指向后面节点没有意义了
+        root.left = null;
+        prev = root;
+    }
 }
