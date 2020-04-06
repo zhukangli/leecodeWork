4a119552811475c12ef5f4717fefa826a61075df
zhukangli <276012105@qq.com>
2020-04-06 15:52:26 +0800

    207题拓扑排序提交

--- src/main/java/com/conley/zhu/leetcode/Code207.java
+++ src/main/java/com/conley/zhu/leetcode/Code207.java
@@ -1,8 +1,51 @@
 package com.conley.zhu.leetcode;
 
-public class Code207 {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+import com.sun.security.auth.UnixNumericGroupPrincipal;
 
-        return false;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 拓扑排序
+ */
+public class Code207 {
+    /**
+     *
+     * @param numCourses    课程数量
+     * @param prerequisites 课程先后顺序数组
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for(int i = 0;i<numCourses;i++){
+            adjacency.add(new ArrayList<>());
+        }
+        int[] indegrees  = new int[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
+        for(int[] cp : prerequisites){
+            //cp[1] -> cp[0],所以cp[0]的入度要+1
+            indegrees[cp[0]]++;
+            //构建邻接表
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        for(int i = 0 ;i<numCourses;i++){
+            //把入度为0的，即初始课程先入队
+            if(indegrees[i] == 0)queue.add(i);
+        }
+        while (!queue.isEmpty()){
+            numCourses--;
+            //获得初始课程
+            int i = queue.poll();
+            //得到初始课程对应的下一节课,把他们入度减少，使他们成为初始课程
+            for(int j : adjacency.get(i)){
+                if(--indegrees[j] == 0){
+                    queue.add(j);
+                }
+            }
+        }
+        return numCourses == 0;
     }
+
 }
